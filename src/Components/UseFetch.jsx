a05c66e4543a53f39e2f537b67fc2590f87e27a9
mjk-hudson <majik.hudson@icloud.com>
2025-10-03 19:34:16 -0400

    Initial Commit

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,5 +1,14 @@
+import { useState, useEffect } from "react";
+
 const UseFetch = (url) => {
-    
+    //Use state variable to store the data
+    const [data,setData] =useState();
+//Use useEffect to fetch data from the url
+    useEffect(() =>{
+        fetch(url).then((res)=>res.json())
+        .then((data) => setData(data))
+    },[])
+    return [data]
 }
 
 export default UseFetch
